279dc06bb58a37e578a790c9930da4bfdca9307c
ben <j00791@reading-school.co.uk>
2016-01-28 11:26:52 +0000

    encoding

--- src/math/Definition.java
+++ src/math/Definition.java
@@ -7,17 +7,26 @@ public class Definition {
 	// y=mx+C
 
 	String name;
-	String  method;
+	String method;
+	String[] terms;
 
 	public Definition(String in) throws IllegalArgumentException{
 		in = in.toLowerCase();
 		if(!in.contains("=")) throw new IllegalArgumentException("Illegal Argument: Missing \"=\"");
 		if(in.contains(" ")) throw new IllegalArgumentException("Illegal Argument: Contains spaces");
+		//if(in.contains("(") || in.contains(")")) throw new IllegalArgumentException("Illegal Argument: Contains brackets");
+		if(in.contains("%") || in.contains("|") || in.contains("⋅") || in.contains("×")||name.contains("±") || name.contains("∓")|| name.contains("÷")|| name.contains("√"))
+			throw new IllegalArgumentException("Illegal Argument: Illegal operators");
 		String[] parts =in.split("=",2);
 		name = parts[0];
-		if(name.contains("*")||name.contains("/")||name.contains("+")||name.contains("-")) throw new IllegalArgumentException("Illegal Argument: Variable contains operators");
+		if(name.contains("*")||name.contains("/")||name.contains("+")||name.contains("-") )
+			throw new IllegalArgumentException("Illegal Argument: Variable contains operators");
 		method  = parts[1];
 		if(method.contains("=")) throw new IllegalArgumentException("Illegal Argument: Definition contains to many \"=\"");
+
+		terms = method.split("((?<=[+-/*^])|(?=[+-/*^]))");
+
+		for(String t:terms)	System.out.print(t+",");
 
 	}
 }
--- src/math/Solver.java
+++ src/math/Solver.java
@@ -4,14 +4,24 @@ public class Solver {
 
 	Var var;
 	Definition[] defs;
+	Var[] knowns;
 
-	public Solver(Var v, Definition[] d){
+	public Solver(Var v, Definition[] d, Var[] n){
 		v = var;
+		int x = 0;
 		for(int i = 0; i<d.length;i++){
 			if(d[i].name.equals(var)){
-
-
+				x++;
 			}
 		}
+		defs = new Definition[x];
+		x = 0;
+		for(int i = 0; i<d.length;i++){
+			if(d[i].name.equals(var)){
+				defs[x] = d[i];
+				x++;
+			}
+		}
+
 	}
 }
